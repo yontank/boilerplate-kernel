6db2bdd47cc26072f1628ad7b7e078ecb6c2a97c
yontank <yontank007@gmail.com>
2025-07-07 23:00:14 +0300

    I FINALLY UNDERSTAND NOW

--- boot.asm
+++ boot.asm
@@ -1,8 +1,39 @@
-ORG 0x7c00
-BITS 16
+; ORG 0
+; BITS 16
+;
 
+_start:
+	jmp short start
+	nop
+
+times 33 db 0
 
 start:
+	jmp 0x7c0:step2
+
+
+int0: 
+	mov ah, 0eh
+	mov al, 'V'
+	mov bx, 0x00
+	int 0x10
+	iret
+
+step2:
+	cli ; clear Interrupts
+	mov ax, 0x7c0
+	mov ds, ax
+	mov es, ax
+	mov ax, 0x00
+	mov ss, ax
+	mov sp, 0x7c00
+	sti ; Enables Interrupts
+
+	mov word[ss:0x00], int0
+	mov word[ss:0x02], 0x7c0
+	int 0
+
+
 	mov si, message
 	call print
 	mov bx, 0
